e75a0a87a6cb74f7ebf57fcf3b42038e2bbf2e65
Hallelujah Kebede <matiashalleluya19@gmail.com>
2025-10-01 20:47:53 +0300

    remove unused packages

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from flask import Flask , render_template
+from flask import Flask
 from google.cloud import discoveryengine
      
 app = Flask(__name__)
