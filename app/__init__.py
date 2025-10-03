59c381011372a9acfd0b4fbaf769570ba669e3a1
Hallelujah Kebede <matiashalleluya19@gmail.com>
2025-10-03 04:57:51 +0300

    Get reasoning agent fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import vertexai
+from vertexai.preview import reasoning_engines
 
 from flask import Flask
 from google.cloud import discoveryengine
@@ -17,6 +18,8 @@ NB_R_ENGINE_LOCATION = os.getenv('NB_R_ENGINE_LOCATION')
 STAGING_BUCKET = os.getenv('STAGING_BUCKET')
 
 MAX_RETRIES = 10
+vertexai.init(project=PROJECT_ID, location=NB_R_ENGINE_LOCATION, staging_bucket=STAGING_BUCKET)
+
 
 @app.route("/")
 def start():
@@ -44,14 +47,7 @@ def search_db(query):
     result = [str(response.document) for response in responses]
     return result
     
-
-vertexai.init(project=PROJECT_ID, location=LOCATION, staging_bucket=STAGING_BUCKET)
-client = vertexai.Client(
-    project=PROJECT_ID,
-    location=LOCATION
-)
-
-remote_agent = client.agent_engines.get(name="projects/{PROJECT_ID}/locations/{NB_R_ENGINE_LOCATION}/reasoningEngines/{NB_R_ENGINE_ID}")
+remote_agent = reasoning_engines.ReasoningEngine(reasoning_engine_name=NB_R_ENGINE_ID)
 
 @app.route("/ask_gemini/<query>")
 def ask_gemini(query):
