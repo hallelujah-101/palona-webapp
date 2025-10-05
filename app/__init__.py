d0742ea271012596fa925ffabea7e2ee2d4f1ff5
Hallelujah Kebede <matiashalleluya19@gmail.com>
2025-10-05 05:12:34 +0300

    Use  json inputs in endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 import os
+import logging
 import requests
 import vertexai
 from vertexai.preview import reasoning_engines
 
-from flask import Flask
+from flask import Flask, request
 from google.cloud import discoveryengine
 from dotenv import load_dotenv
+from google.cloud import discoveryengine_v1 as discoveryengine
+from google.api_core.client_options import ClientOptions
+
      
 app = Flask(__name__)
 
@@ -16,40 +20,53 @@ LOCATION = os.getenv('LOCATION_ID')
 NB_R_ENGINE_ID = os.getenv('NB_R_ENGINE_ID')
 NB_R_ENGINE_LOCATION = os.getenv('NB_R_ENGINE_LOCATION')
 STAGING_BUCKET = os.getenv('STAGING_BUCKET')
+APP_ID = os.getenv('APP_ID')
 
-MAX_RETRIES = 10
+
 vertexai.init(project=PROJECT_ID, location=NB_R_ENGINE_LOCATION, staging_bucket=STAGING_BUCKET)
 
+def search_sample(project_id: str, location: str, engine_id: str, search_query: str):
+    client_options = (
+        ClientOptions(api_endpoint=f"{location}-discoveryengine.googleapis.com")
+        if location != "global"
+        else None
+    )
+
+    client = discoveryengine.SearchServiceClient(client_options=client_options)
+
+    serving_config = f"projects/{project_id}/locations/{location}/collections/default_collection/engines/{engine_id}/servingConfigs/default_config"
+
+    request = discoveryengine.SearchRequest(
+        serving_config=serving_config,
+        query=search_query,
+        page_size=10,
+        )
+    
+    response = client.search(request)
+
+    return response
 
 @app.route("/")
 def start():
     return "Listening on port 8080"
 
-@app.route("/search_db/<query>")
-def search_db(query):
+@app.route("/search_db", methods=['POST'])
+def search_db():
+    data = request.json
+    query = data.get('query')
+    print(query)
     
-    client = discoveryengine.SearchServiceClient()
-    
-    service_config = client.serving_config_path(
-        project=PROJECT_ID,
-        location=LOCATION,
-        data_store=os.getenv('DATA_STORE_ID'),
-        serving_config=os.getenv('APP_ID')
-    )
-
-    request = discoveryengine.SearchRequest(
-        serving_config=service_config,
-        query=query,
-        page_size=1
-    )
-    
-    responses = client.search(request)
+    responses = search_sample(PROJECT_ID, LOCATION, APP_ID, query)
     result = [str(response.document) for response in responses]
     return result
     
 remote_agent = reasoning_engines.ReasoningEngine(reasoning_engine_name=NB_R_ENGINE_ID)
 
-@app.route("/ask_gemini/<query>")
-def ask_gemini(query):
+@app.route("/ask_gemini", methods=['POST'])
+def ask_gemini():
+    prompt_data = request.json
+    query = prompt_data.get('query')
+    print(query)
+    
     response = remote_agent.query(input=query)
     return response['output']
