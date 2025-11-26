d1fb47445bbcab0a8e06607011a3f564978a4a0e
hallelujah-101 <hallelujahapplications@gmail.com>
2025-11-26 10:36:54 +0300

    ADK approach

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
+import logging
 from app.agent.reasoning_agent import AGENT
 from app.utils.helper_functions import *
 
 from flask import Flask, request
+
 
 app = Flask(__name__)
 
@@ -10,22 +12,24 @@ def start():
     return f"Listening on port 8080"
 
 @app.route("/ask_gemini", methods=['POST', 'OPTIONS'])
-def ask_gemini():
-    """Calls a Reasoning Agent to generate a response from the query"""
+async def ask_gemini():
+    """Query a reasoning Agent to generate a response from the query"""
 
+    empty_response = response_object(" ")
     if request.method == 'OPTIONS':
-        return response_object(" ")
-    
+        return empty_response
+        
     session_id = request.form.get('session_id')
     text = request.form.get('text')
     attachments = request.form.get('attachments')
 
-    query = construct_query(text, [attachments])
-    configuration = construct_configuration(session_id)
-    
     agent = AGENT()
-    model_response = agent.query(query, configuration)
-    formatted_output = format_response(model_response)
+    model_response = await agent.query(text, session_id)
     
-    response = response_object(formatted_output)
+    response = None
+    if model_response:
+        response = response_object(model_response)
+    else:
+        response = empty_response
+        
     return response
--- app/agent/reasoning_agent.py
+++ app/agent/reasoning_agent.py
@@ -1,22 +1,42 @@
 import os
 from dotenv import load_dotenv
 
+from google.adk.agents import Agent
 import vertexai
-from vertexai.preview import reasoning_engines
+from vertexai import agent_engines
 
 class AGENT:
 
     def __init__(self):
+        """Initialises the vertex ai project and reasoning engine"""
         load_dotenv()
 
         PROJECT_ID = os.getenv('PROJECT_ID')
         LOCATION = os.getenv('LOCATION')
         STAGING_BUCKET = os.getenv('STAGING_BUCKET')
         AGENT_ENGINE_RESOURCE_NAME = os.getenv('AGENT_ENGINE_RESOURCE_NAME')
-
+        
         vertexai.init(project=PROJECT_ID, location=LOCATION, staging_bucket=STAGING_BUCKET)
+        
+        agents_list = list(vertexai.agent_engines.list())
+        if agents_list:
+            self.remote_agent = agents_list[0]  
+            client = agent_engines
+            print(f"✅ Connected to deployed agent: {self.remote_agent.resource_name}")
+        else:
+            print("❌ No agents found. Please deploy first.")
 
-        self.remote_agent = reasoning_engines.ReasoningEngine(AGENT_ENGINE_RESOURCE_NAME)
-
-    def query(self, query, configuration):
-        return self.remote_agent.query(input=query, config=configuration)
+    async def query(self, query, user_id): 
+        
+        async for event in self.remote_agent.async_stream_query(
+            message=query,
+            user_id=user_id,
+        ):
+            
+            if 'content' in event.keys() and ('parts' in event['content'].keys()):
+                for part in event['content']['parts']:
+                    if 'function_response' in part.keys():
+                        if part['function_response']['name'] == 'output_formatter':
+                            return part['function_response']['response']
+        
+        return None
