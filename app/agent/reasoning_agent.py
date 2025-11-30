ea0d6e7bee7d1435f646200639d08e1b23a5c214
hallelujah-101 <hallelujahapplications@gmail.com>
2025-11-30 14:11:06 +0300

    Add other agent

--- app/agent/reasoning_agent.py
+++ app/agent/reasoning_agent.py
@@ -10,9 +10,8 @@ from google.adk.sessions import VertexAiSessionService
 from google.adk.tools.preload_memory_tool import PreloadMemoryTool
 from google.adk.agents.callback_context import CallbackContext
 from google.genai import types
-from google.adk.agents import Agent
+from google.adk.agents import Agent, LlmAgent
 from google.adk.runners import Runner
-
 
 class AGENT:
 
@@ -29,28 +28,37 @@ class AGENT:
         self.APP_ID = os.getenv('APP_ID')
         self.AGENT_ENGINE_ID = os.getenv('AGENT_ENGINE_ID')
         self.MODEL = os.getenv('MODEL_NAME')
-                
+
         os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "TRUE"
         os.environ["GOOGLE_CLOUD_PROJECT"] = self.PROJECT_ID
         os.environ["GOOGLE_CLOUD_LOCATION"] = self.LOCATION
         
-        self.agent_instruction = """ You are a helpful and funny conversational agent named Gemi, and you are a Gemini.
-                            1. You provide users with recommendations and search results from a catalogue using the vertex_search tool. 
-                            2. You take user questions which may or may not contain images and if they are product related search through a database using the vertex_search tool which allows for text and image search, if they aren't product related you respond with a general response.
-                            3. You use the output_formatter tool to, when the response from the vertex_search tool has products, by extracting the ProductTitle, Gender, ProductId and Category fields from the result for each product
-                            4. You use the output_formatter tool to form the final response for all queries and just use the text part of the response if the response doesn't contain products
+        search_agent_instruction = """You are a helpful research agent. You provide the customer_support_agent with raw search results from a catalogue using the vertex_search tool. You do not give the customer_support_agent details of any intermediate steps and data"""
+        
+        customer_agent_instruction = """You are a helpful and funny conversational agent named Gemi, and you are a Gemini.
+                            1. You take user questions which may or may not contain images and if they are product related you search through a database using the catalogue_search_agent which allows for text and image search, if they aren't product related you respond with a general response.
+                            2. You take the response from the catalogue_query_agent and use the output_formatter tool to, when the response from the catalogue_search_agent has products, by extracting the ProductTitle, Gender, ProductId and Category fields from the result for each product
+                            3. You use the output_formatter tool to form the final response for all queries and just use the text part of the your response if it's a general response or the the response from the catalogue_query_agent doesn't contain products
                             You do not give the user details of any intermediate steps and data"""
         
         vertexai.init(project=self.PROJECT_ID, location=self.LOCATION, staging_bucket=self.STAGING_BUCKET)
         
-        self.agent = Agent(
+        catalogue_search_agent = Agent(
                             model=self.MODEL,
                             name="catalogue_query_agent",
-                            tools=[PreloadMemoryTool(), self.vertex_search, self.output_formatter],
-                            after_agent_callback=self.add_session_to_memory,
-                            instruction=self.agent_instruction,
-                        )
-                        
+                            tools=[self.vertex_search],
+                            instruction=search_agent_instruction,
+        )
+        
+        self.agent = Agent(
+                        model=self.MODEL,
+                        name="customer_support_agent",
+                        tools=[PreloadMemoryTool(), self.output_formatter],
+                        after_agent_callback=self.add_session_to_memory,
+                        sub_agents=[catalogue_search_agent],
+                        instruction=customer_agent_instruction,
+        )
+        
         self.session_service = VertexAiSessionService(
             project=self.PROJECT_ID, location=self.LOCATION, agent_engine_id=self.AGENT_ENGINE_ID
         )
@@ -66,9 +74,6 @@ class AGENT:
             memory_service=self.memory_service,
         )
         
-        
-        
-        
     async def add_session_to_memory(self, callback_context: CallbackContext) -> Optional[types.Content]:
         """Automatically save completed sessions to memory bank """
         if hasattr(callback_context, "_invocation_context"):
@@ -78,25 +83,22 @@ class AGENT:
                     invocation_context.session
                 )
     
-    async def query(self, query, attachments, user_id): 
+    async def query(self, query, user_id): 
         session = await self.session_service.create_session(
             app_name=self.agent.name, 
             user_id=user_id,
         )
-        content = types.Content(role="user", parts=[types.Part(text=query), types.Part(content=attachments)])
+        content = types.Content(role="user", parts=[types.Part(text=query)])
         events = self.runner.run(
             user_id=session.user_id, session_id=session.id, new_message=content
         )
 
-        for event in events:
-            if event.is_final_response():
-                final_response = event.content.parts[0].text
-                return f"Agent Response: {final_response}"
-            
+        for event in events:    
             if event.content:
                 if event.content.parts[0].function_response:
                     if event.content.parts[0].function_response.name == 'output_formatter':
                         if 'error' in event.content.parts[0].function_response.response.keys():
+                            print(event.content.parts[0].function_response.response.response)
                             continue
                         else:
                             return event.content.parts[0].function_response.response
@@ -136,7 +138,7 @@ class AGENT:
                 responses.extend([str(response.document) for response in search_result])
 
         else:
-
+            
             request = discoveryengine.SearchRequest(
                     serving_config=serving_config,
                     query=search_query,
